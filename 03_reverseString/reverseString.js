3e7b6ae17050e4e6a589e8a6b7f84913c77e994d
AdemBendjama <93732841+AdemBendjama@users.noreply.github.com>
2022-12-07 18:01:07 +0100

    Solve Exo 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let arrayOfChars = word.split('');
+    let reverseWord = '';
+    for (let i = arrayOfChars.length - 1 ; i >= 0 ; i--) {
+        reverseWord = reverseWord + arrayOfChars[i];
+        
+    }
 
+    return reverseWord ;
 };
 
+console.log(reverseString("hello"));
 // Do not edit below this line
 module.exports = reverseString;
