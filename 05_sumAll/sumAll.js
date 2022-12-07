7c6a2fb7974f4078fb7f59be7818cca1b1a25761
AdemBendjama <93732841+AdemBendjama@users.noreply.github.com>
2022-12-07 21:47:17 +0100

    Solve Exo 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+    //
+    if (start < 0 || end < 0) return "ERROR";
+    if (isNaN(start) || isNaN(end)) return "ERROR";
+    if ((typeof start) === 'string' || (typeof end) === 'string') return "ERROR";
 
+    //
+    let sum = 0;
+    if (start > end) {
+        let tmp = start;
+        start = end;
+        end = tmp;
+    }
+
+
+    //
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+
+    //
+    return sum;
 };
 
 // Do not edit below this line
