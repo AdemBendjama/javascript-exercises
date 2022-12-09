79078faabddc5ac4c73a94834f7edd3355a87b76
AdemBendjama <93732841+AdemBendjama@users.noreply.github.com>
2022-12-09 22:02:57 +0100

    Solve Exo 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function (nbr) {
+    //
+    if (nbr < 0) return "OOPS";
 
+    if (nbr == 1 || nbr == 2) {
+        return 1;
+    } else if (nbr == 0) {
+        return 0;
+    } else {
+        return fibonacci(nbr - 1) + fibonacci(nbr - 2);
+    }
 };
 
 // Do not edit below this line
