04cec2dda2f23c55bc1c26d859f62f3290cd2900
AdemBendjama <93732841+AdemBendjama@users.noreply.github.com>
2022-12-09 15:08:18 +0100

    Solve Exo 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,55 @@
-const add = function() {
-	
+const add = function (nbr1, nbr2) {
+    return (nbr1) + (nbr2);
 };
 
-const subtract = function() {
-	
+const subtract = function (nbr1, nbr2) {
+    return (nbr1) - (nbr2);
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+    let result = 0;
+    array.forEach(element => {
+        result += (element);
+    });
+
+    return result;
 };
 
-const multiply = function() {
+const multiply = function (array) {
+    if (array.length == 0) return 0;
+    let result = 1;
+    array.forEach(element => {
+        result *= (element);
+    });
 
+    return result;
 };
 
-const power = function() {
-	
+const power = function (nbr, power) {
+    if (power == 0) return 0;
+    let result = 1;
+    for (let i = 1; i <= power; i++) {
+        result *= nbr;
+    }
+
+    return result;
 };
 
-const factorial = function() {
-	
+const factorial = function (nbr) {
+
+    if (nbr == 0) {
+        return 1;
+    } else {
+        return nbr * factorial(nbr - 1);
+    }
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+    add,
+    subtract,
+    sum,
+    multiply,
+    power,
+    factorial
 };
