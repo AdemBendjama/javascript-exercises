4ef4d0fae01df69e5ee705243df8836cb6af25dd
AdemBendjama <93732841+AdemBendjama@users.noreply.github.com>
2022-12-09 21:56:08 +0100

    Solve Exo 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,24 @@
-const palindromes = function () {
+const palindromes = function (phrase) {
+    let punctuations = [";", ":", ",", "!", ".", "?", "'", '"'," "];
+    punctuations.forEach(element => {
+        phrase = phrase.split(element);
+        let combineStrings = "";
+        phrase.forEach(string => {
+            combineStrings += string;
+        });
+        phrase = combineStrings ;
+    });
 
+    phrase = phrase.toLowerCase();
+    phrase = phrase.split("");
+
+    for (let i = 0; i < phrase.length / 2; i++) {
+        if (phrase[i] != phrase[phrase.length - i - 1]) {
+            return false;
+        }
+
+    }
+    return true;
 };
 
 // Do not edit below this line
