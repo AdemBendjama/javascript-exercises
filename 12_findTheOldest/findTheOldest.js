ed6309557885cb5cc7143730e6a169d3217bbec2
AdemBendjama <93732841+AdemBendjama@users.noreply.github.com>
2022-12-09 22:21:58 +0100

    Solve Exo 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,44 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    //
+    let oldestAge = 0; 
+    let oldestPerson ;
+    let age ;
+    people.forEach(element => {
+        if(element.yearOfDeath == undefined){
+            //
+            age = (new Date().getFullYear()) - element.yearOfBirth;
+            if(age>oldestAge){
+                oldestAge=age;
+                oldestPerson ={
+                    name: element.name,
+                    yearOfBirth:element.yearOfBirth
+                };
+            }
+        }else{
+            //
+            age = element.yearOfDeath - element.yearOfBirth;
+            if(age>oldestAge){
+                oldestAge=age;
+                oldestPerson ={
+                    name: element.name,
+                    yearOfBirth:element.yearOfBirth
+                };
+            }
+        }
+        
+    }); 
+
+
+    return oldestPerson;
+
 
 };
-
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 2018,
+    }
+]
+findTheOldest(people);
 // Do not edit below this line
 module.exports = findTheOldest;
